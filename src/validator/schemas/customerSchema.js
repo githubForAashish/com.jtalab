3915f47f8cd939ff2de0781ca55715fe56ff0eb8
Aashish Bhandari <aashish.dux@gmail.com>
2024-03-22 20:53:32 +0545

    Allow null fixes

--- src/validator/schemas/customerSchema.js
+++ src/validator/schemas/customerSchema.js
@@ -1,12 +1,12 @@
 const Joi = require('joi');
 
 const createCustomer = Joi.object({
-    company_name: Joi.string().required(),
-    phone_number: Joi.string().max(10).required(),
+    company_name: Joi.string().allow(null),
+    phone_number: Joi.string().max(10).allow(null),
     address: Joi.string().allow(null),
-    contact_person: Joi.string().required(),
-    pan: Joi.string().required(),
-    email: Joi.string().email().required(),
+    contact_person: Joi.string().allow(null),
+    pan: Joi.string().allow(null),
+    email: Joi.string().email().allow(null),
 });
 
 const updateCustomer = Joi.object({
